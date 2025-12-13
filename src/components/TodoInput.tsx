942b1eced796ce0d991099cbd4cc3f34256e496c
Dominik Windorpski <windorpskidominik2204@gmail.com>
2025-12-13 12:46:55 +0100

    feature: icon for add todo button

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,11 +1,13 @@
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 export default function TodoInput() {
   return (
     <div className="flex items-center">
       <Input className="rounded-r-none p-[20px] px-4 text-lg" type="text" placeholder="What needs to be done?" />
-      <Button className="rounded-l-none p-[21px] text-lg" type="submit">Add Todo</Button>
+      <Button className="flex items-center justify-center gap-1 rounded-l-none p-[21px] text-lg" type="submit">Add Todo<FontAwesomeIcon icon={faPlus} /></Button>
     </div>
   );
 }
