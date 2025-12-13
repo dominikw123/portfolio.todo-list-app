17d567a1d101a3a4fa8da84d5df0cc4b9cf527ca
Dominik Windorpski <windorpskidominik2204@gmail.com>
2025-12-13 12:06:14 +0100

    feature: todo list wrapper component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import TodoListWrapper from "./components/TodoListWrapper";
+
 export default function App() {
   return (
-    <></>
+    <main className="p-0 flex items-center justify-center w-full min-h-screen overflow-x-hidden sm:p-10">
+      <TodoListWrapper>
+        <h1>Todo List</h1>
+      </TodoListWrapper>
+    </main>
   );
 }
