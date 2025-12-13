16e6e26c5bad4c0a4d27105477df567e972d4489
Dominik Windorpski <windorpskidominik2204@gmail.com>
2025-12-13 12:51:05 +0100

    style: changed todo input padding and font size

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -6,8 +6,8 @@ import { faPlus } from "@fortawesome/free-solid-svg-icons";
 export default function TodoInput() {
   return (
     <div className="flex items-center">
-      <Input className="rounded-r-none p-[20px] px-4 text-lg" type="text" placeholder="What needs to be done?" />
-      <Button className="flex items-center justify-center gap-1 rounded-l-none p-[21px] text-lg" type="submit">Add Todo<FontAwesomeIcon icon={faPlus} /></Button>
+      <Input className="rounded-r-none p-[19px] px-4 font-medium" type="text" placeholder="What needs to be done?" />
+      <Button size="lg" className="flex items-center justify-center gap-1 rounded-l-none" type="submit">Add Todo<FontAwesomeIcon icon={faPlus} /></Button>
     </div>
   );
 }
