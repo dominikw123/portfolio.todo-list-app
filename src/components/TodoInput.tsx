220a0b5a17e0e81eac6e5bfa348b1e9668ab61a5
Dominik Windorpski <windorpskidominik2204@gmail.com>
2025-12-13 12:40:23 +0100

    style: changed todo input size and placeholder

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -4,8 +4,8 @@ import { Button } from "./ui/button";
 export default function TodoInput() {
   return (
     <div className="flex items-center">
-      <Input className="rounded-r-none" type="text" placeholder="Add a new todo" />
-      <Button className="rounded-l-none" type="submit">Add Todo</Button>
-    </div >
+      <Input className="rounded-r-none p-[20px] px-4 text-lg" type="text" placeholder="What needs to be done?" />
+      <Button className="rounded-l-none p-[21px] text-lg" type="submit">Add Todo</Button>
+    </div>
   );
 }
